9d0334f9dac9debc85d52e8b5c1e6dbb6f9d987a
wanghj_dz <169068671@qq.com>
2025-10-31 19:28:59 +0800

    更新GeoGebra离线资源包

--- geogebra/test-location/GeoGebra/HTML5/5.0/web/deferredjs/ACB971B618CEE337E8EF6FD1235D6BAE/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.web.runAsyncCallback2('OCc(4063,1,DQn);_.Hd=function(){};r$m(Rc)(2);\n//# sourceURL=web-2.js\n')
--- geogebra/test-location/GeoGebra/HTML5/5.0/webSimple/deferredjs/DED2DCE7CEF6A61737CAB084C1D69A5E/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.webSimple.runAsyncCallback2('Lvb(2282,1,q6h);_.hd=function(){};ewh(Rc)(2);\n//# sourceURL=webSimple-2.js\n')
